280a66b8ccaf8cdc8b72712a66ec9623741c35d4
duawptjd99 <48855410+duawptjd99@users.noreply.github.com>
2023-06-30 19:18:16 +0900

    clean: 필요없는 코드 삭제
    실수로 print문까지 올렸음

--- board/src/main/java/com/example/board/controller/BoardController.java
+++ board/src/main/java/com/example/board/controller/BoardController.java
@@ -52,7 +52,6 @@ public class BoardController {
     @GetMapping("/home/board/update")
     public String showUpdateBoard(@RequestParam("id") Long id, Model model) {
         model.addAttribute("boardDto", this.boardService.findById(id));
-        System.out.println("asd");
         return "board-update";
     }
 
