ed239d890bde885c6749f29810ae512c0876a824
duawptjd99 <48855410+duawptjd99@users.noreply.github.com>
2023-07-17 02:35:21 +0900

    Change: 회원가입 페이지 form js로 수정

--- board/src/main/resources/static/js/signup.js
+++ board/src/main/resources/static/js/signup.js
@@ -1,18 +1,72 @@
-function checkSignUp() {
-    var userPw = document.getElementById("user-pw").value;
-    var reconfirmPw = document.getElementById("reconfirm-pw").value;
-    var userId = document.getElementById("user-id").value;
-    var userName = document.getElementById("user-name").value;
-    var userEmail = document.getElementById("user-email").value;
-    var pattern = /^[\w\.-]+@[\w\.-]+\.\w+$/;
+let eMailPattern = /^[\w\.-]+@[\w\.-]+\.\w+$/;
 
-    if ((userId !== "" && userPw !== "" && userName !== ""
-        && reconfirmPw !== "")
-        && (userPw === reconfirmPw)
-        && pattern.test(userEmail)) {
-        return true;
-    } else {
-        alert("형식에 맞게 작성하여 주세요.");
+class SignUp {
+
+    userPw;
+    reconfirmPw;
+    userId;
+    userName;
+    userEmail;
+    signUpButton;
+    signInButton;
+    signUpForm;
+
+    constructor() {
+        this.userPw = document.getElementById("user-pw");
+        this.reconfirmPw = document.getElementById("reconfirm-pw");
+        this.userId = document.getElementById("user-id");
+        this.userName = document.getElementById("user-name");
+        this.userEmail = document.getElementById("user-email");
+        this.signUpButton = document.getElementById("signup-button");
+        this.signInButton = document.getElementById("signin-button");
+        this.signUpForm = document.getElementById("signup-form");
+
+        this.signInButton.addEventListener("click", this.signIn);
+
+        this.signUpButton.addEventListener("click", this.submitDeleteForm);
+    }
+
+    submitDeleteForm = (event) => {
+        event.preventDefault();
+        var errorMessage = this.checkSignUp();
+        if (this.resultSignUp(errorMessage)) {
+            this.signUpForm.action = "/signup";
+            this.signUpForm.method = "POST";
+            this.signUpForm.submit();
+        }
+    }
+    signIn = (event) => {
+        event.preventDefault();
+        window.location.href = "/";
+    }
+
+    checkSignUp = () => {
+
+        let msg = "";
+        if (this.userName.value === "") {
+            msg = "이름이 작성되지 않았습니다.";
+        }else if (this.userId.value === "") {
+            msg = "아이디가 작성되지 않았습니다.";
+        }else if (this.userEmail.value === "") {
+            msg = "이메일이 작성되지 않았습니다.";
+        }else if (!eMailPattern.test(this.userEmail.value)) {
+            msg = "이메일 형식이 올바르지 않습니다.";
+        }else if (this.userPw.value === "") {
+            msg = "비밀번호가 작성되지 않았습니다.";
+        }else if (this.reconfirmPw.value !== this.userPw.value) {
+            msg = "비밀번호가 일치하지 않습니다.";
+        }
+
+        return msg;
+    }
+
+    resultSignUp = (msg) => {
+        if (msg === "") {
+
+            return true;
+        }
+        alert(msg);
+
         return false;
     }
 }
