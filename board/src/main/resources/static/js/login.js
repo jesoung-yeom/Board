15eeeac1ff5e4bce1b51831721e2e686a4d18a1b
duawptjd99 <48855410+duawptjd99@users.noreply.github.com>
2023-07-17 02:34:47 +0900

    change: 로그인 페이지 form js 수정

--- board/src/main/resources/static/js/login.js
+++ board/src/main/resources/static/js/login.js
@@ -1,21 +1,48 @@
+class SignIn {
 
-function checkSignIn() {
-    const userId = document.getElementById("user-id").value;
-    const userPw = document.getElementById("user-pw").value;
+    userId;
+    userPw;
+    signUpButton;
+    signInButton;
+    signInForm;
 
-    if (userId.trim() === '' && userPw.trim() === '') {
-        alert('아이디와 비밀번호를 입력해주세요.');
+    constructor() {
+        this.userId = document.getElementById("user-id");
+        this.userPw = document.getElementById("user-pw");
+        this.signUpButton = document.getElementById("signup-button");
+        this.signInForm = document.getElementById("login-form");
+        this.signInButton = document.getElementById("signin-button")
 
-        return false;
-    } else if (userId.trim() === '') {
-        alert('아이디를 입력해주세요.');
+        this.signUpButton.addEventListener("click", this.signUp);
+        this.signInButton.addEventListener("click", this.submitSignInForm);
+    }
 
-        return false;
-    } else if (userPw.trim() === '') {
-        alert('비밀번호를 입력해주세요.');
+    submitSignInForm = (event) => {
+        event.preventDefault();
+        if (this.checkSignIn()) {
+            this.signInForm.action = "/signin";
+            this.signInForm.method = "POST";
+            this.signInForm.submit();
+        }
+    }
 
-        return false;
-    } else {
+    signUp = (event) => {
+        event.preventDefault();
+        window.location.href = "/signup";
+    }
+
+    checkSignIn = () => {
+        if (this.userId.value === "") {
+            alert("아이디가 작성되지 않았습니다.");
+
+            return false;
+        }
+
+        if (this.userPw.value === "") {
+            alert("비밀번호가 작성되지 않았습니다.");
+
+            return false;
+        }
 
         return true;
     }
